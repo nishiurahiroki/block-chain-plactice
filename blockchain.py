8e74f5db949259de6a9551c0bb7ab71285d35d14
nishiurahiroki <nishiurahiroki@gmail.com>
2017-12-27 13:59:19 +0000

    add endpoints.

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,11 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+
+from flask import Flask
+
 
 class BlockChain(object):
     def __init__(self):
@@ -29,6 +34,20 @@ class BlockChain(object):
         })
         return self.lastBlock['index'] + 1
 
+    def proofOfWork(self, laskProof):
+        proof = 0
+        while self.valid_proof(lastProof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def validProof(lastProof, proof):
+        guess = f'{lastProof}{proof}'.encode()
+        guessHash = hashlib.sha256(guess).hexdigest()
+        print(guessHash[:4] == '0000')
+        print(guessHash)
+        return guessHash[:4] == '0000'
+
     @property
     def lastBlock(self):
         return self.chain[-1]
@@ -37,3 +56,30 @@ class BlockChain(object):
     def hash(block):
         blockString = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(blockString).hexdigest()
+
+
+
+app = Flask(__name__)
+
+nodeIdentifier str(uuid4()).replace('-', '')
+
+blockchain = BlockChain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # TODO
+    return 'mining'
+
+@app.route('/transactions/new', methods=['POST'])
+def newTransaction():
+    # TODO
+    return 'new transactions'
+
+@app.route('/chain', methods=['GET']):
+def fullChain():
+    response = {
+        'chain'  : blockchain.chain,
+        'length' : len(blockchain.chain)
+    }
+    return jsonify(response), 200
